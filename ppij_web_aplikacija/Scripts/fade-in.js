ff5820545715c698725e91453400d2a0a26ee2e5
mjanjic1 <matej.janjic@fer.hr>
2016-05-14 12:21:00 +0200

    dovršen redizajn početne stranice

---
+++ ppij_web_aplikacija/Scripts/fade-in.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    elementFade();
+
+    $(window).scroll(function () {
+        elementFade();
+    });
+
+});
+
+var elementFade = function () {
+    $('.fade-in-element').each(function (i) {
+
+        var middle_of_object = $(this).offset().top + $(this).outerHeight() / 2;
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        if (bottom_of_window > middle_of_object) {
+
+            $(this).animate({ 'opacity': '1' }, 600);
+
+        }
+
+    });
+}
---
+++ ppij_web_aplikacija/Scripts/parallax-scroll.js
@@ -0,0 +1,17 @@
+﻿window.requestAnimationFrame = window.requestAnimationFrame
+    || window.mozRequestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.msRequestAnimationFrame
+    || function (f) { setTimeout(f, 1000 / 60) };
+
+var pageTitle = $('#page-title');
+var viewportHeight = window.innerHeight * 0.50;
+
+function parallax() {
+    var scrolltop = window.pageYOffset;
+    pageTitle.css('padding-top', (viewportHeight - scrolltop * .35)); //35% bitch!
+}
+
+window.addEventListener('scroll', function () {
+    requestAnimationFrame(parallax);
+}, false);
